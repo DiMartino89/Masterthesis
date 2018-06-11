9487876485c19924d2dc4932ef83003f7bdd6a45
DiMartino89 <spitfire-24@web.de>
2018-06-11 23:49:13 +0200

    [THESIS-03] add controller-scripts for cam and player and set basic player-sounds

---
+++ Assets/Scripts/Player/CameraController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+	// Camera variables
+	private enum RotationAxis {
+		mouse_x = 1,
+		mouse_y = 2
+	}
+	
+	private RotationAxis axis = RotationAxis.mouse_x;
+	
+	private float min_vertical = -45.0f;
+	private float max_vertical = 45.0f;
+	
+	private float sens_horizontal = 5.0f;
+	private float sens_vertical = 5.0f;
+	
+	private float rotation_x = 0;
+	private float rotation_y = 0;
+	
+	/*
+	 *	Function: 		CameraController
+	 *	Description:	Spawn the player always on a random position at the bottom and set first person view
+	 *	Date:			19.04.2018
+	 *	Source:			https://www.youtube.com/watch?v=Ov9ekwAGhMA
+	 *	Functionality:
+	 *	1.	...
+	 *	2.	...
+	 */
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(axis == RotationAxis.mouse_x) {
+			transform.Rotate(0, Input.GetAxis("Mouse X") * sens_horizontal, 0);
+		} else if(axis == RotationAxis.mouse_y) {
+			rotation_x -= Input.GetAxis("Mouse Y") * sens_vertical;
+			rotation_x = Mathf.Clamp(rotation_x, min_vertical, max_vertical);
+			rotation_y = transform.localEulerAngles.y;
+			
+			transform.localEulerAngles = new Vector3(rotation_x, rotation_y, 0);
+		}
+	}
+}
---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	// Player variables
+	public GameObject flashLight;
+	public AudioClip running;
+	public AudioClip light;
+	
+	private float speed = 4.0f;
+	private CharacterController controller;
+	private AudioSource audio;
+	private int health = 100;
+	
+	//public float speed = 3.0F;
+    //public float rotateSpeed = 3.0F;
+	
+	void Start () {
+		controller = GetComponent<CharacterController>();
+		audio = GetComponent<AudioSource>();
+	}
+	
+	void Update () {
+		
+		controller = GetComponent<CharacterController>();
+		
+		// Movement and Look
+		float delta_x = Input.GetAxis("Horizontal") * speed;
+		float delta_z = Input.GetAxis("Vertical") * speed;
+		Vector3 movement = new Vector3(delta_x, 0, delta_z);
+		
+		//movement *= Time.deltaTime;
+		movement = transform.TransformDirection(movement);
+		controller.SimpleMove(movement);
+		
+		// Running
+		if(Input.GetButton("Horizontal") || Input.GetButton("Vertical")) {
+			if(!audio.isPlaying) {
+				float volume = Random.Range(0.1f, 0.5f);
+				audio.pitch = 1.0f;
+				audio.PlayOneShot(running, volume);
+				audio.enabled = true;
+				audio.loop = false;
+			}
+		}
+		
+		// CodingBox
+		if (Input.GetKeyDown(KeyCode.Space)) {
+			
+			GameObject codeBox_panel = GameObject.Find("CodingBoxPanel");
+			BoxSlider box_slider = codeBox_panel.GetComponent<BoxSlider>();
+			
+			box_slider.toggleBox();
+		}
+		
+		// Flashlight
+		if (Input.GetKeyDown(KeyCode.L)) {
+			if(!flashLight.activeSelf) {
+				flashLight.SetActive(true);
+				audio.pitch = 1.0f;
+				audio.PlayOneShot(light, 1.0f);
+			} else {
+				flashLight.SetActive(false);
+				audio.pitch = 1.0f;
+				audio.PlayOneShot(light, 1.0f);
+			}
+		} 
+	}
+}
